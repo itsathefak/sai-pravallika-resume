a92265403bf028bdcfa62b12b7e8f4f113c36abc
itsathefak <athefayubkhan@gmail.com>
2025-06-28 01:46:13 -0400

    Update Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,38 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sai Pravallika Allu | Sustainability & Architecture",
-  description: "Portfolio of Sai Pravallika Allu - Architect & Sustainability Expert",
-    generator: 'v0.dev'
-}
+  description:
+    "Portfolio of Sai Pravallika Allu - Architect & Sustainability Expert",
+  generator: "v0.dev",
+  icons: {
+    icon: "/SP.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
